d5e826efcd8b2c7e9af607a9feeba21e89cd9de8
ioppermann <ingo@oppermann.ch>
2018-07-10 22:10:58 +0200

    Move cmake file to root

---
+++ distclean.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+FILES="cmake_install.cmake CMakeCache.txt CMakeFiles install_manifest.txt Makefile modjpeg-dynamic src/contrib/modjpeg-static"
+DIRS="./"
+
+for dir in ${DIRS}; do
+	for file in ${FILES}; do
+		if [ ! -e ${dir}${file} ]; then
+			continue
+		fi
+
+		rm -rf ${dir}${file}
+	done
+done
+
+rm *.dylib
+rm *.so
